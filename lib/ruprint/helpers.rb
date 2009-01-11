53378f964b4446d46221666ae4585327bee36450
Jon Gilbraith <jon@professionalnerd.com>
2009-01-11 15:20:16 +0000

    Added a helper to generate the stylesheet link tags for you.

--- lib/ruprint/helpers.rb
+++ lib/ruprint/helpers.rb
@@ -10,5 +10,17 @@ module Ruprint #:nodoc:
       builder.render
     end
 
+    # Adds the appropriate stylesheet link tags to your layout. One option:
+    #  - css_dir (defaults to /stylesheets) to designate where to load the stylesheets from.
+    def blueprint_link_tags(options = {})
+      options[:css_dir] ||= "/stylesheets"
+
+      out  = stylesheet_link_tag("#{options[:css_dir]}/screen", :media => "screen, projection")
+      out += stylesheet_link_tag("#{options[:css_dir]}/print", :media => "print")
+      out += stylesheet_link_tag("#{options[:css_dir]}/application")
+      out += "<!--[if IE]>#{stylesheet_link_tag("#{options[:css_dir]}/ie", :media => "screen, projection")}<![endif]-->"
+      out
+    end
+
   end
 end
